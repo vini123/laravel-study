763c566d5c62f4f72b05b50a5c9682bf81a63047
vini123 <lichking_lin86@qq.com>
2018-03-26 08:34:22 +0000

    日志的使用，配置请看config/app.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -12,6 +12,7 @@ class PagesController extends Controller
         Log::warning('遗忘就和记得一样，是送给彼此最好的纪念。');
         Log::error('爱从来都不是归宿，也不是我们彼此的救赎。');
 
+        Log::notice('仿佛有痛楚。如果我晕眩，那是因为幻觉丰盛，能量薄弱。足以支持我对你的迷恋，不够支持我们的快乐。');
         return 'log is created';
     }
 }
